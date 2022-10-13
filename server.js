753ad101fcf03885c31e4130a36aac049b51896d
Michael Nguyen <nguyenmichaelm@yahoo.com>
2022-10-13 09:50:09 -0700

    connect database to app

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,7 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default respons for any other request (Not Found)
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'data',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => { 
+  console.log(rows);
+});
+
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
